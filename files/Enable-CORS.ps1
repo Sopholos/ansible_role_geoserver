40161a9413cfe158768175f1d3f022e7fa7dd126
Tomilov Alexander <sopholos@sopholos.ru>
2024-10-28 14:05:50 +0500

    INFRA-615 cors comment changes

--- files/Enable-CORS.ps1
+++ files/Enable-CORS.ps1
@@ -9,7 +9,7 @@ function Remove-Comment($text, $startComment, $endComment) {
     $startIndex = $text.IndexOf($startComment)
     if ($startIndex -lt 0) { return $text; }
 
-    $endIndex = $text.IndexOf($endComment, $startIndex)
+    $endIndex = $text.IndexOf($endComment, $startIndex + $startComment.Length)
     if ($endIndex -lt 0) { return $text; }
 
     $text = $text.Remove($endIndex, $endComment.Length)
@@ -19,9 +19,21 @@ function Remove-Comment($text, $startComment, $endComment) {
 
 $text = Get-Content $path | Out-String
 
+# changed coment way in new geoserver (2.25.3 at least)
+
+$text = Remove-Comment `
+    $text `
+    "<!-- Uncomment following filter to enable CORS in Jetty. Do not forget the second config block further down. -->`n    <!--" `
+    "-->"
+
 $text = Remove-Comment `
     $text `
     "<!-- Uncomment following filter to enable CORS in Jetty. Do not forget the second config block further down." `
+    "-->"
+
+$text = Remove-Comment `
+    $text `
+    "<!-- Uncomment following filter-mapping to enable CORS -->`n    <!--" `
     "-->"
 
 $text = Remove-Comment `
